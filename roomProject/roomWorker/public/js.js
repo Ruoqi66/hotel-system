684f27123f268bd8854aa6fe5623e27976f0ccf7
Ruoqi66 <2791109145@qq.com>
2026-06-09 19:10:53 +0800

    完整版

--- roomProject/roomWorker/public/js.js
+++
@@ -1,6 +0,0 @@
-fetch('../public/sidebar.html')
-        .then(response => response.text())
-        .then(html => {
-            document.getElementById('sidebar-container').innerHTML = html;
-        })
-        .catch(error => console.error('加载Header失败:', error));
